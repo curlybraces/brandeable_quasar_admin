5acf0d05ae626c5e8c2e28aa895107c27c466a85
djavierq <dijaq08@gmail.com>
2020-09-01 09:50:07 -0500

    changes

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 import {LocalStorage} from "quasar";
 
 Vue.prototype.$axios = axios.create({
-  baseURL: 'http://localhost:8000',
-  //baseURL: 'https://brandeable.pe'
+  //baseURL: 'http://localhost:8000',
+  baseURL: 'https://brandeable.pe'
   /*headers: {
     'Access-Control-Allow-Origin': '*',
     'Authorization': LocalStorage.getItem("token"),
